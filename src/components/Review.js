e2654020e8aa5cd42ef89009bd4bd39ecf7139db
Karan09542 <karan09542@gmail.com>
2025-10-20 11:50:44 +0530

    Review Project

---
+++ src/components/Review.js
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+
+const Review = ({ reviews }) => {
+  const [index, setIndex] = useState(0);
+  const { author, job, text, image, id } = reviews[index];
+
+  function handlePrev() {
+    const prevIndex = (index - 1 + reviews.length) % reviews.length;
+    setIndex(prevIndex);
+  }
+  function handleNext() {
+    const nextIndex = (index + 1) % reviews.length;
+    setIndex(nextIndex);
+  }
+
+  function supriseMe() {
+    const randIndex = Math.floor(Math.random() * reviews.length);
+    setIndex(randIndex);
+  }
+
+  return (
+    <div>
+      <div className="review" style={{ width: "clamp(200px, 100%, 500px)", margin: "auto" }}>
+        <h1 id={`author-${id}`}>{author}</h1>
+        <p>{job}</p>
+        <img
+          style={{ width: "100%", height: "500px", objectFit: "cover" }}
+          src={image}
+          alt="image"
+        />
+        <p>{text}</p>
+        <button className="prev-btn" onClick={handlePrev}>
+            previous
+        </button>
+        <button className="next-btn" onClick={handleNext}>
+            next
+        </button>
+        <button className="random-btn" onClick={supriseMe}>
+            surprise me
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Review;
